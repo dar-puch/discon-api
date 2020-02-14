a439a64d99be5b599b6e2f979933025f6fb0e774
Daria Puchnarewicz <Daria.Puchnarewicz@stepstone.com>
2020-02-14 12:15:12 +0100

    refactor with doFetch function

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,108 @@
-describe('Hello, Enzyme!', () => {
-  it('renders', () => {
+import { addToHistory, getHistory, getReleaseById, removeFromHistory, search } from '../index';
+//zmienić kod źródłowy, żeby funkcje się nie powtarzały i dostosować do tego testy (wtedy też się nie będą powtarzać).
+function mockFetch(response: any) {
+  return jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => response
+    })
+  );
+}
 
+let searchResponse = [
+  {
+    id: 2,
+    cover_image: 'patch.to.image',
+    title: 'Shafter High Concert Band - Shafter High School',
+    catno: '282K',
+    year: '1982',
+    artist: 'Shafter',
+  }
+];
+
+let historyResponse = {
+  queryId: '216d',
+  date: '2020-01-23',
+  parameters: {
+    queryString: 'Shafter',
+    what: 'artist',
+  },
+  result: searchResponse,
+}
+let failedResponse = {
+  ok: false,
+  status: '500'
+}
+
+const expectProperFetchUrl = (url: string) => {
+  expect(window.fetch).toHaveBeenCalledWith(`http://localhost:4000/${url}`);
+}
+
+describe('search', () => {
+
+  it('should call fetch with the correct url', () => {
+    window.fetch = mockFetch(searchResponse);
+    search('Shafter', 'artist');
+    expectProperFetchUrl('data/?artist=Shafter');
+  });
+
+  it('returns correct answer', () => {
+    //dodać przypadek że nie ma fetcha
+    window.fetch = mockFetch(searchResponse);
+    expect(search('Shafter', 'artist')).resolves.toBe(searchResponse);
+  });
+
+  it('the search fails with an error', async () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve(failedResponse)
+    });
+    try {
+      await search('asd', 'artist');
+    } catch (e) {
+      expect(e).toEqual(Error('500'));
+    }
+  })
+});
+
+describe('getReleaseById', () => {
+  it('should call fetch with the correct url', () => {
+    window.fetch = mockFetch(searchResponse);
+    getReleaseById(2);
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:4000/data/2');
+  });
+
+  it('returns correct answer', () => {
+    window.fetch = mockFetch(searchResponse);
+    expect(getReleaseById(2)).resolves.toBe(searchResponse);
+  });
+
+  it('the search fails with an error', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve(failedResponse)
+    });
+    expect(getReleaseById(2)).rejects.toThrow('500');
   })
 
-})
+});
+
+describe('getHistory', () => {
+  it('should call fetch with the correct url', () => {
+    window.fetch = mockFetch(historyResponse);
+    getHistory();
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:4000/history');
+  });
+
+  it('returns correct answer', () => {
+    window.fetch = mockFetch(historyResponse);
+    expect(getHistory()).resolves.toBe(historyResponse);
+  });
+
+});
+
+describe('addToHistory', () => {
+
+});
+
+describe('removeFromHistory', () => {
+
+});
--- src/index.ts
+++ src/index.ts
@@ -21,74 +21,110 @@ export interface HistoryItem {
   result: Release[];
 }
 
+
+const headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+}
+
+// export const search = async (query: string, what: what): Promise<Release[]> => {
+//   const response = await fetch(`http://localhost:4000/data/?${what}=${query}`);
+//   if (response.ok) {
+//     const json = await response.json();
+//     return json;
+//   }
+//   else {
+//     throw Error(`${response.status}`);
+//   }
+// }
+
 export const search = async (query: string, what: what): Promise<Release[]> => {
-  const response = await fetch(`http://localhost:4000/data/?${what}=${query}`);
-  if (response.ok) {
-    const json = await response.json();
-    return json;
-  }
-  else {
-    throw Error(`${response.status}`);
-  }
+  return doFetch(`data/?${what}=${query}`, 'GET', headers, null);
 }
 // search('Nirvana', 'artist');
 
 
+// export const getReleaseById = async (id: number): Promise<Release> => {
+//   const response = await fetch(`http://localhost:4000/data/${id}`);
+//   if (response.ok) {
+//     const json = await response.json();
+//     return json;
+//   }
+//   else {
+//     throw Error(`${response.status}`);
+//   }
+// }
 export const getReleaseById = async (id: number): Promise<Release> => {
-  const response = await fetch(`http://localhost:4000/data/${id}`);
-  if (response.ok) {
-    const json = await response.json();
-    return json;
-  }
-  else {
-    throw Error(`${response.status}`);
-  }
+  return doFetch(`data/${id}`, 'GET', headers, null);
 }
 
+// export const getHistory = async (): Promise<HistoryItem[]> => {
+//   const response = await fetch(`http://localhost:4000/history`);
+//   if (response.ok) {
+//     const json = await response.json();
+//     return json;
+//   }
+//   else {
+//     throw Error(`${response.status}`);
+//   }
+// };
 export const getHistory = async (): Promise<HistoryItem[]> => {
-  const response = await fetch(`http://localhost:4000/history`);
-  if (response.ok) {
-    const json = await response.json();
-    return json;
-  }
-  else {
-    throw Error(`${response.status}`);
-  }
-};
-
-export const addToHistory = async (item: HistoryItem): Promise<void> => {
-  const response = await fetch(`http://localhost:4000/history`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(item)
-  });
-  if (response.ok) {
-    const json = await response.json();
-    console.log(json);
-  }
-  else {
-    throw Error(`${response.status}`);
-  }
+  return doFetch('history', 'GET', headers, null);
 }
+
+// export const addToHistory = async (item: HistoryItem): Promise<void> => {
+//   const response = await fetch(`http://localhost:4000/history`, {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(item)
+//   });
+//   if (response.ok) {
+//     const json = await response.json();
+//     console.log(json);
+//   }
+//   else {
+//     throw Error(`${response.status}`);
+//   }
+// }
+export const addToHistory = async (item: HistoryItem): Promise<void> => {
+  doFetch('history', 'POST', headers, `JSON.stringify(${item})`);
+}
+
+// export const removeFromHistory = async (id: HistoryItem['queryId'] | 'all'): Promise<void> => {
+//   const response = await fetch(`http://localhost:4000/history/?id=${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     }
+//   });
+//   if (response.ok) {
+//     const json = await response.json();
+//     console.log(json);
+//   }
+//   else {
+//     throw Error(`${response.status}`);
+//   }
+// }
 
 export const removeFromHistory = async (id: HistoryItem['queryId'] | 'all'): Promise<void> => {
-  const response = await fetch(`http://localhost:4000/history/?id=${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+  doFetch(`history/?id=${id}`, 'DELETE', headers, '');
+}
+
+export const doFetch = async (urlTail: any, method: string, headers: HeadersInit, body: string | null): Promise<any> => {
+  const response = await fetch(`http://localhost:4000/${urlTail}`, {
+    method: method,
+    headers: headers,
+    body: body
   });
   if (response.ok) {
     const json = await response.json();
-    console.log(json);
+    return json;
   }
   else {
     throw Error(`${response.status}`);
   }
 }
-
-
